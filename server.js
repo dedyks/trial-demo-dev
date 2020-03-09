b33df934f18d36777885b18d857ac8ab05f42813
Dedy <dedyjoomla@gmail.com>
2020-03-10 04:22:47 +0700

    try

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
  */
 var os = require('os');
 require('custom-env').env('staging')
-
+// mongoose-morgan
+var mongooseMorgan = require('mongoose-morgan');
 
 
 var http = require('http');
@@ -33,6 +34,20 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(require('express-status-monitor')())
 
+// Logger
+app.use(mongooseMorgan({
+  collection: 'error_logger',
+  connectionString: 'mongodb://localhost:27017/logs-db',
+  user: 'root',
+  pass: 'rootpassword'
+ },
+ {
+  skip: function (req, res) {
+      return res.statusCode < 400;
+  }
+ },
+ 'dev'
+));
 app.use(express.static(path.join(__dirname, "public")));
 
 /**
